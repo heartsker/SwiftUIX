44614450b5a75b80b9adc679ca7d8eba083139cf
heartsker <heartsker@gmail.com>
2021-09-23 10:42:46 +0300

    initial commit

---
+++ SwiftUIX/SwiftUIX.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIX.h
+//  SwiftUIX
+//
+//  Created by Daniel Pustotin on 23.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIX.
+FOUNDATION_EXPORT double SwiftUIXVersionNumber;
+
+//! Project version string for SwiftUIX.
+FOUNDATION_EXPORT const unsigned char SwiftUIXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIX/PublicHeader.h>
+
+
---
+++ SwiftUIXExample/SwiftUIXExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIXExampleApp.swift
+//  SwiftUIXExample
+//
+//  Created by Daniel Pustotin on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIXExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
