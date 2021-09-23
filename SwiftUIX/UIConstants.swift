0e95c738e91a12dc537541984e7e021936fc9dbc
heartsker <heartsker@gmail.com>
2021-09-23 10:50:57 +0300

    added constants

---
+++ SwiftUIX/UIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  UIConstants.swift
+//  SwiftUIX
+//
+//  Created by Daniel Pustotin on 23.09.2021.
+//
+
+import SwiftUI
+
+// MARK: - Screen sizes
+public let screen: CGSize = UIScreen.main.bounds.size
+public let screen_half: CGSize = CGSize(width: screen.width / 2, height: screen.height / 2)
+
+// MARK: - Color sets
+public let prettyColors: [Color] = [.blue, .orange, .pink, .purple, .white]
+public let allColors: [Color] = [.black, .blue, .gray, .green, .orange, .pink, .purple, .red, .white]
