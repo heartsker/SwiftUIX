517724e80296d06ba68bcee362bd809170f22fa9
heartsker <heartsker@gmail.com>
2021-09-23 11:16:06 +0300

    added constants tests

--- SwiftUIXTests/UIConstantsTests.swift
+++ SwiftUIXTests/UIConstantsTests.swift
@@ -1,14 +1,13 @@
 //
-//  SwiftUIXTests.swift
+//  UIConstantsTests.swift
 //  SwiftUIXTests
 //
 //  Created by Daniel Pustotin on 23.09.2021.
 //
 
 import XCTest
-@testable import SwiftUIX
 
-class SwiftUIXTests: XCTestCase {
+class UIConstantsTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
