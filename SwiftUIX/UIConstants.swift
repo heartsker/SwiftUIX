3b51510864f4335773512aac3aaae41822682822
heartsker <heartsker@gmail.com>
2021-09-24 16:49:16 +0300

    added view extentions and constants

--- SwiftUIX/UIConstants.swift
+++ SwiftUIX/UIConstants.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 // MARK: - Screen sizes
 public let screen: CGSize = UIScreen.main.bounds.size
-public let screen_half: CGSize = CGSize(width: screen.width / 2, height: screen.height / 2)
+public let screenReversed = CGSize(width: -screen.width, height: -screen.height)
+public let screenHalf: CGSize = CGSize(width: screen.width / 2, height: screen.height / 2)
+public let screenHalfReversed: CGSize = CGSize(width: -screen.width / 2, height: -screen.height / 2)
+
 public func screen(ratio: CGFloat) -> CGSize {
 	CGSize(width: screen.width * ratio, height: screen.height * ratio)
+}
+
+// MARK: - Corners
+public let cornerRadiusBig: CGFloat = 25.0
+public let cornerRadiusMedium: CGFloat = 15.0
+public let cornerRadiusSmall: CGFloat = 10.0
+public let shadowRadiusBig: CGFloat = 20.0
+public let shadowRadiusMedium: CGFloat = 10.0
+public let shadowRadiusSmall: CGFloat = 5.0
+
+// MARK: - Spacing
+public let spacingBig: CGFloat = screen.width / 5
+public let spacingMedium: CGFloat = screen.width / 10
+public let spacingSmall: CGFloat = screen.width / 15
+
+// MARK: - Angle
+public func angle(_ derees: Double, CW: Bool = false) -> Angle {
+	Angle(degrees: derees * (CW ? -1 : 1))
 }
 
 // MARK: - Color sets
